4a8c95973ac22788f3cdf77292d13fb0171bb3b4
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2024-07-28 13:00:58 +0530

    Implement auth, appwrite, and sentry

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -78,15 +78,16 @@ import Link from 'next/link';
 import React from 'react';
 import BankCard from './BankCard';
 
-interface RightSidebarProps {
-  user: {
-    firstName: string;
-    lastName: string;
-    email: string;
-  };
-  transactions: any[];
-  banks: any[];
-}
+// interface RightSidebarProps {
+//   user: {
+//     name: any;
+//     firstName: string;
+//     lastName: string;
+//     email: string;
+//   };
+//   transactions: any[];
+//   banks: any[];
+// }
 
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
   return (
@@ -97,12 +98,12 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
         <div className='profile'>
           <div className='profile-img'>
             <span className='text-5xl font-bold text-blue-500'>
-              {user.firstName[0]}
+              {user.name[0]}
             </span>
           </div>
           <div className='profile-details'>
             <h1 className='profile-name'>
-              {user.firstName} {user.lastName}
+              {user.name}
             </h1>
             <p className='profile-email'>
               {user.email}
@@ -132,7 +133,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
               <BankCard 
                 key={banks[0].$id}
                 account={banks[0]}
-                userName={`${user.firstName} ${user.lastName}`}
+                userName={user.name}
                 showBalance={false}
               />
             </div>
@@ -141,7 +142,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
                 <BankCard
                   key={banks[1].$id}
                   account={banks[1]}
-                  userName={`${user.firstName} ${user.lastName}`}
+                  userName={user.name}
                   showBalance={false}
                 />
               </div>
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://1100929d3903126bca74c8f346845595@o4507497627320320.ingest.us.sentry.io/4507497642131456",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
